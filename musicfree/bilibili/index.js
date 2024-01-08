5657d349f151e99ff637e7686d514b32dd03946b
EmdayLive <326236950@qq.com>
2024-01-08 08:20:44 +0000

    更新 'musicfree/bilibili/index.js'

--- musicfree/bilibili/index.js
+++ musicfree/bilibili/index.js
@@ -431,7 +431,7 @@ module.exports = {
     author: '猫头猫',
     defaultSearchType: "album",
     cacheControl: "no-cache",
-    srcUrl: "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/bilibili/index.js",
+    srcUrl: "https://agit.ai/EmdayLive/Emdaytv/raw/branch/master/musicfree/bilibili/index.js",
     primaryKey: ["id", "aid", "bvid", "cid"],
     hints: {
         importMusicSheet: [
