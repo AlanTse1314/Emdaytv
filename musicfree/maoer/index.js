132512008c4b292f9b3743080aa353773f7364f2
EmdayLive <326236950@qq.com>
2024-01-08 07:50:10 +0000

    更新 'musicfree/maoer/index.js'

--- musicfree/maoer/index.js
+++ musicfree/maoer/index.js
@@ -180,7 +180,7 @@ module.exports = {
     author: '猫头猫',
     version: "0.1.4",
     appVersion: ">0.1.0-alpha.0",
-    srcUrl: "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/maoerfm/index.js",
+    srcUrl: "https://agit.ai/EmdayLive/Emdaytv/raw/branch/master/musicfree/maoer/index.js",
     cacheControl: "no-cache",
     supportedSearchType: ["music", "album",],
     async search(query, page, type) {
