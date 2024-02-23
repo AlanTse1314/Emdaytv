06abc27cadb3ec3256a19067ab18869952eb660a
EmdayLive <326236950@qq.com>
2024-02-23 09:22:46 +0000

    更新 'blili/js/lib/种子短剧.js'

--- blili/js/lib/种子短剧.js
+++ blili/js/lib/种子短剧.js
@@ -5,7 +5,7 @@ var rule = {
     searchUrl: '/api.php/provide/vod?ac=detail&wd=**&pg=fypage',
     detailUrl: '/api.php/provide/vod?ac=detail&ids=fyid',
     url: '/api.php/provide/vod?ac=detail&t=fyclass&pg=fypage&f=',
-    class_name: '短剧&KS&DY&都市&古装&重生&逆袭&虐恋&萌宝&言情&穿越&战神&神医&赘婿&甜宠&其他',
+    class_name: '短剧&快手短剧&抖音短剧&都市&古装&重生&逆袭&虐恋&萌宝&言情&穿越&战神&神医&赘婿&甜宠&其他',
     class_url: '1&2&3&4&5&6&7&8&9&10&11&12&13&14&15&16',
     tab_rename:{'zzdj':'Emday'}, 
     推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
